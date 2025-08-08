09727f87876a12b6c13a85175437272dcadeb209
PiotrKrolak <krolak.piotr.lca@gmail.com>
2025-08-08 20:11:48 +0200

    keep going with NumPy

--- 01-data-basics/numpy_basics.py
+++ 01-data-basics/numpy_basics.py
@@ -13,8 +13,10 @@ b = np.array([1,2,3,4,5])
 c = np.array([[6,5,4],[3,2,1]])
 # 3d array
 d = np.array([[[1,2,3],[4,5,6]],[[123,234,345],[456,567,678]]])
+# n-d array
+e = np.array([1,2,3,4,5],  float)
 
-arrays_names = [a, b, c,d]
+arrays_names = [a, b, c, d, e]
 
 for arr in arrays_names:
     print(arr)
@@ -37,6 +39,38 @@ print("\nSTEP: ")
 print(b[1:4: 2])
 print(b[::3])
 
+# Shape of an Array
+print("\nShape of an Array: ")
+print(c.shape)
 
-# https://www.w3schools.com/python/numpy/numpy_data_types.asp
+# Reshape array from 1-d to...
+arr = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+
+new_array = arr.reshape(4,3)
+new_array_2 = arr.reshape(2, 3, 2)
+
+print(f"\nReshape array from {arr} to: \n{new_array}")
+print(f"\nReshape array from {arr} to: \n{new_array_2}")
+
+
+# Flattening the arrays
+# Flattening array means converting a multidimensional array into a 1D array.
+# We can use reshape(-1) to do this.
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
+
+print("\nChange 2d array to 1d array: ")
+print(newarr)
+
+
+# enumerate
+print("\nenumerate 2d: ")
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
+
+arr = np.array([1, 2, 3])
+
+print("\nenumerate 1d: ")
+for idx, x in np.ndenumerate(arr):
+  print(idx, x)
 
