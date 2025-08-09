d500cfe138bbe7dbdea40913e3eede294f927e82
PiotrKrolak <krolak.piotr.lca@gmail.com>
2025-08-10 00:41:43 +0200

    finish of NumPy basics

--- 01-data-basics/numpy_basics.py
+++ 01-data-basics/numpy_basics.py
@@ -74,3 +74,98 @@ print("\nenumerate 1d: ")
 for idx, x in np.ndenumerate(arr):
   print(idx, x)
 
+
+# Stack function
+# z dwóch macierzy jednowymiarowych tworzy jedną w ktorej kazdemu wymiarowi odpowiada n'ta pozycja w pierwotnej macierzy
+arr1 = np.array([1, 2, 3])
+
+arr2 = np.array([4, 5, 6])
+
+arr = np.stack((arr1, arr2), axis=1)
+
+print("\nStack function: ")
+print(f"\nArray 1: \n{arr1}")
+print(f"\nArray 2: \n{arr2}")
+print(f"\nStack array 1 and array 2: \n{arr}")
+
+# hstack - dodaje dwie jednowymiarowe macierze do jednej jednowymiarowej
+arr = np.hstack((arr1, arr2))
+
+print(f"\nhStack array 1 and array 2: \n{arr}")
+
+#dStack dodaje maciesze o rozmiarze n i tworzy jedna n wymiarowa macierz
+arr = np.dstack((arr1, arr2))
+
+print(f"\ndStack array 1 and array 2: \n{arr}")
+
+#Array Split
+#dziel macierz jednowymiarowa na x macierzy jednowymiarowych
+arr = np.array([1, 2, 3, 4, 5, 6])
+
+print(f"\nArray for split: {arr}")
+
+newarr = np.array_split(arr, 3)
+
+print("\nSplit array: ")
+print(newarr)
+
+# Split the 2-D array into three 2-D arrays.
+print("\nSplit the 2-D array into three 2-D arrays.")
+print(newarr)
+
+# The example above returns three 2-D arrays.
+# In addition, you can specify which axis you want to do the split around.
+# The example below also returns three 2-D arrays, but they are split along the column (axis=1).
+print("\nThe example below also returns three 2-D arrays, but they are split along the column (axis=1).")
+print(newarr)
+
+
+# Searcjing Arrays
+arr = np.array([1, 2, 3, 4, 5, 4, 4])
+
+print(f"\n\nArray for Searching: {arr}")
+
+# Find the indexes where the value is 4:
+value = 4
+x = np.where(arr == value)
+
+print(f"return array of index where value is {value}")
+print(x)
+
+
+# Sort Array
+print("\nSort Array: ")
+arr_num = np.array([3, 2, 0, 1])
+arr_str = np.array(['banana', 'cherry', 'apple'])
+arr_bool = np.array([True, False, True])
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+
+print(f"\nNumeric Array: {arr_num}")
+print("Sorted num array: ")
+print(np.sort(arr_num))
+
+print(f"\nNumeric Array: {arr_str}")
+print("Sorted str array: ")
+print(np.sort(arr_str))
+
+print(f"\nNumeric Array: {arr_bool}")
+print("Sorted bool array: ")
+print(np.sort(arr_bool))
+
+print(f"\nNumeric Array: {arr}")
+print("Sorted arr: ")
+print(np.sort(arr))
+
+
+# Filtering Arrays
+# Create an array from the elements on index there "True":
+
+arr = np.array([41, 42, 43, 44])
+
+x = [True, False, True, False]
+
+newarr = arr[x]
+print(f"\nOryginall array: {arr}")
+print(f"filter x: {x}")
+print(newarr)
