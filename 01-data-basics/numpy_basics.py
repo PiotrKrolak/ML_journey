60e000ba4541453973f428ad47fdbc45f7a011bb
PiotrKrolak <krolak.piotr.lca@gmail.com>
2025-08-10 00:45:46 +0200

    finish of NumPy basics

--- 01-data-basics/numpy_basics.py
+++ 01-data-basics/numpy_basics.py
@@ -169,3 +169,24 @@ newarr = arr[x]
 print(f"\nOryginall array: {arr}")
 print(f"filter x: {x}")
 print(newarr)
+
+
+
+# Create a filter array that will return only values higher than 42:
+# Create an empty list
+filter_arr = []
+
+# go through each element in arr
+for element in arr:
+  # if the element is higher than 42, set the value to True, otherwise False:
+  if element > 42:
+    filter_arr.append(True)
+  else:
+    filter_arr.append(False)
+
+newarr = arr[filter_arr]
+
+print("\nCreate a filter array that will return only values higher than 42:")
+
+print(f"Array: {arr}")
+print(f"Filter array: {filter_arr}")
